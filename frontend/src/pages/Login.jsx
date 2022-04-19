936510022e60738628a2aeb7889daa7a28e92762
fsjiongco <fsjiongco@up.edu.ph>
2022-04-19 18:29:14 +0800

    Added the privacy notice title in its section

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,7 +7,9 @@ const Login = () => {
     return <>
     <Row className="mt-5">
         <Col lg={5} md={6} sm={12} className="p-5 m-5">
-            <Container className="p-5 m-5 shadow-sm rounded-lg bg-white"></Container>
+            <Container className="p-5 m-5 shadow-sm rounded-lg bg-white text-dark bg-opacity-50">
+                <h4>Privacy Notice</h4>
+            </Container>
         </Col>
 
         <Col>
