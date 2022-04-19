5dce0c5b39a67b8fa143e79fcd7cc3dd52c16ace
fsjiongco <fsjiongco@up.edu.ph>
2022-04-19 18:02:28 +0800

    Styled the privacy notice section and form section

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     return <>
     <Row className="mt-5">
         <Col lg={5} md={6} sm={12} className="p-5 m-5">
-            <Container className="p-5 m-5 shadow-sm rounded-lg"></Container>
+            <Container className="p-5 m-5 shadow-sm rounded-lg bg-white"></Container>
         </Col>
 
         <Col>
