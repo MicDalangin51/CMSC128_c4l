3f63ff6b08bb3d955770171915f4f9a5a1820359
fsjiongco <fsjiongco@up.edu.ph>
2022-04-19 17:58:20 +0800

    Added the Form section; username, password, login button

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import './Login.css';
-import {Row, Col, Container} from "react-bootstrap";
+import {Row, Col, Container, Form, Button} from "react-bootstrap";
 
 const Login = () => {
     return <>
     <Row className="mt-5">
         <Col lg={5} md={6} sm={12} className="p-5 m-5">
             <Container className="p-5 m-5 shadow-sm rounded-lg"></Container>
+        </Col>
+
+        <Col>
+            <Container className="mt-5 p-5">
+                <Form className="login-form">
+                    <Form.Group controlId="formUsername" className="w-50">
+                        <Form.Label className="input-label">Username</Form.Label>
+                        <Form.Control title="Enter username"/>
+                    </Form.Group>
+                    <br/>
+                    <Form.Group controlId="formPassword" className="w-50">
+                        <Form.Label className="input-label">Password</Form.Label>
+                        <Form.Control type="password" title="Enter password"/>
+                    </Form.Group>
+
+                    <br/>
+                    <Button variant="outline-light w-50" type="submit" className="login-button">
+                        Login
+                    </Button>
+                </Form>
+            </Container>
         </Col>
     </Row>
     </>;
