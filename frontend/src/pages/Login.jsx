7e2a345938232c7e4ecee364379f1f2fc76381df
fsjiongco <fsjiongco@up.edu.ph>
2022-04-19 17:35:10 +0800

    Added the privacy notice section

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './Login.css';
+import {Row, Col, Container} from "react-bootstrap";
 
 const Login = () => {
     return <>
+    <Row className="mt-5">
+        <Col lg={5} md={6} sm={12} className="p-5 m-5">
+            <Container className="p-5 m-5 shadow-sm rounded-lg"></Container>
+        </Col>
+    </Row>
     </>;
   };
 
