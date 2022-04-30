aa8aa25705877d1ab81b2b3b29ee6a45ad322488
garthendrich <garthendrich@gmail.com>
2022-04-30 16:56:52 +0800

    Add sample api route with mock data

--- backend/app.py
+++ backend/app.py
@@ -9,3 +9,38 @@ app = Flask(__name__, static_folder=f"{client_dist_folder}/assets")
 @app.route('/<path:path>')
 def index(path=""):
     return send_from_directory(client_dist_folder, "index.html")
+
+
+@app.route('/api/students/')
+@app.route('/api/students/<int:page>')
+def students(page=1):
+
+    print("student directory page number:", page)
+
+    # Mock data
+    students = [
+        {
+            "name": "Ronn Jiongco",
+            "email": "rjiongco@up.edu.ph",
+            "status": "verified",
+        },
+        {
+            "name": "Garth Lapitan",
+            "email": "glapitan@up.edu.ph",
+            "status": "unverified",
+        },
+        {
+            "name": "Jemuel Juatco",
+            "email": "jjuatco@up.edu.ph",
+            "status": "verified",
+        },
+        {
+            "name": "Nathan Muncal",
+            "email": "nmuncal@up.edu.ph",
+            "status": "unverified",
+        },
+    ]
+
+    students *= 25
+
+    return {"students": students}
