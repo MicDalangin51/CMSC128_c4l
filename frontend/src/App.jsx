61dd72e62a97c54491d25011d9103db8def5af51
fsjiongco <fsjiongco@up.edu.ph>
2022-05-05 14:36:06 +0800

    added several routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,25 @@
 import { StudentDirectory } from "/src/pages";
 import { Changelog } from "/src/pages";
 import { Settings } from "/src/pages";
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import { StudentRecord } from "/src/pages";
+import { Login } from "/src/pages";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-    <Router>
-    <div className="content">
+      <Router>
+        <div className="content">
           <Routes>
-            <Route path="/" element={<StudentDirectory/>}></Route>
-            <Route path="/change-log" element={<Changelog/>}></Route>
-            <Route path="/settings" element={<Settings/>}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/logout" element={<Login />}></Route>
+            <Route path="/" element={<StudentDirectory />}></Route>
+            <Route path="/change-log" element={<Changelog />}></Route>
+            <Route path="/settings" element={<Settings />}></Route>
+            <Route path="/student-record" element={<StudentRecord />}></Route>
           </Routes>
-    </div>
-
-    </Router>
+        </div>
+      </Router>
     </>
   );
 };
