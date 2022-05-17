10b2f7bfa6b602f68e4ad5f3aaabd5d537e24f2d
fsjiongco <fsjiongco@up.edu.ph>
2022-05-17 22:31:42 +0800

    exports Register

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,3 +3,4 @@ export { default as StudentDirectory } from "./StudentDirectory";
 export { default as Changelog } from "./Changelog";
 export { default as Settings } from "./Settings";
 export { default as StudentRecord } from "./StudentRecord";
+export { default as Register } from "./Register";
