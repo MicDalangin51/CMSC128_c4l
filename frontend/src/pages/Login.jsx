f0e381337649c6f5f289c8468cfce616219b7ab5
fsjiongco <fsjiongco@up.edu.ph>
2022-05-23 14:59:36 +0800

    changed imports

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Button, Container, Form } from "react-bootstrap";
 
 import "./Login.css";
 import logo from "../images/cas-logo.png";
