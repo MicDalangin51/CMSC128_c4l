98fe100907b0a3df31ab3f8c17dff83a303f98c3
fsjiongco <fsjiongco@up.edu.ph>
2022-05-29 03:46:10 +0800

    created verification modal for verifiers component

---
+++ frontend/src/components/ChangeVerificationModal.jsx
@@ -0,0 +1,58 @@
+import { Modal, Button } from "react-bootstrap";
+
+const ChangeVerificationModal = ({
+  showModal,
+  closeModal,
+  verifier,
+  shac_member,
+  student_num,
+}) => {
+  const handleClose = () => {
+    closeModal();
+  };
+
+  //edits the student
+  function editStudent() {
+    const data = {
+      student_id: student_num,
+      new_data: shac_member,
+      column: verifier,
+    };
+
+    fetch(`/api/students/${studentNumber}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((body) => {
+        console.log(body);
+
+        if (body.success) {
+          alert("Successfully changed!");
+        } else {
+          alert("Failed to change!");
+        }
+      });
+
+    closeModal();
+  }
+
+  return (
+    <Modal size="lg" show={showModal} centered>
+      <Modal.Body>Change {verifier}?</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={editStudent}>
+          Yes
+        </Button>
+        <Button variant="secondary" onClick={handleClose}>
+          No
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ChangeVerificationModal;
