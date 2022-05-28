7f0db0b51dfab15188cb547542fb75cee06c7b3d
garthendrich <garthendrich@gmail.com>
2022-05-28 18:50:33 +0800

    Implement file upload for adding students

--- frontend/src/components/AddStudentModal.jsx
+++ frontend/src/components/AddStudentModal.jsx
@@ -1,8 +1,29 @@
 import { useState } from "react";
-import { Modal, Tabs, Tab, Form, FloatingLabel, Row, Col, Button, Stack } from "react-bootstrap";
+import { Modal, Tabs, Tab, Form, FloatingLabel, Row, Col, Button, Stack, Alert } from "react-bootstrap";
 
 const AddStudentModal = ({ show, closeAddStudentModal }) => {
   const [tabKey, setTabKey] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const submitFileHandler = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", event.target.file.files[0]);
+
+    const response = await fetch("/api/students:file", {
+      method: "POST",
+      body: formData,
+    });
+
+    switch (response.status) {
+      case 200:
+        closeAddStudentModal();
+        break;
+      default:
+        setAlertMessage("Adding students was unsuccessful");
+    }
+  };
 
   return (
     <Modal
@@ -19,12 +40,15 @@ const AddStudentModal = ({ show, closeAddStudentModal }) => {
       <Modal.Body>
         <Tabs onSelect={(key) => setTabKey(key)}>
           <Tab eventKey="csv" title="Upload CSV" className="p-4">
-            <Form>
+            <Form onSubmit={submitFileHandler}>
               <Form.Group controlId="formFile" className="mb-3">
-                <Form.Control type="file" required />
-                </Form.Group>
+                <Form.Control type="file" name="file" accept=".csv,.xls,.xlsx" required />
+              </Form.Group>
+              {alertMessage !== "" && <Alert variable="danger">{alertMessage}</Alert>}
               <Stack direction="horizontal">
-                <Button className="ms-auto">Upload</Button>
+                <Button type="submit" className="ms-auto">
+                  Upload
+                </Button>
               </Stack>
             </Form>
           </Tab>
