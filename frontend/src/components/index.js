1799b0dca9afe916e6fea365e33562cf40e1c588
fsjiongco <fsjiongco@up.edu.ph>
2022-05-30 00:56:58 +0800

    exported EditStudentModal

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,3 +2,4 @@ export { default as DashboardLayout } from "./DashboardLayout";
 export { default as SideBar } from "./SideBar";
 export { default as AddStudentModal } from "./AddStudentModal";
 export { default as ChangeVerificationModal } from "./ChangeVerificationModal";
+export { default as EditStudentModal } from "./EditStudentModal";
