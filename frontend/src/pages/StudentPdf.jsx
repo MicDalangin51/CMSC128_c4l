579d78942283bf6dfa54610fb9a60d627f73f5a0
garthendrich <garthendrich@gmail.com>
2022-06-07 15:54:11 +0800

    Forward refs for StudentPdf

--- frontend/src/pages/StudentPdf.jsx
+++ frontend/src/pages/StudentPdf.jsx
@@ -1,9 +1,11 @@
-const StudentPdf = () => {
+import { forwardRef } from "react";
+
+const StudentPdf = forwardRef((props, ref) => {
   return (
-    <>
+    <div {...props} ref={ref}>
       <h1>pdf display</h1>
-    </>
+    </div>
   );
-};
+});
 
 export default StudentPdf;
